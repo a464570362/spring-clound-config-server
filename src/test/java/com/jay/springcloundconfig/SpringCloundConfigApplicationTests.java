1851cc51168603ebb046eae4eaf162d5f22ad210
liushuai@ideal-link.com <shuai515>
2020-03-17 14:51:19 +0800

    Initial commit

---
+++ src/test/java/com/jay/springcloundconfig/SpringCloundConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jay.springcloundconfig;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloundConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
